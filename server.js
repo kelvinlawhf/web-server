28ad0a530f48563f3a9ad9705ba24ae6fa0759cc
Kelvin Law <kelvinlaw_hf@hotmail.com>
2017-02-12 10:18:04 -0800

    Updated port

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 var express = require('express');
-var PORT = 3000;
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-// app.get('/',function(req,res){
-// 	res.send("Hello Express");
-// });
+var middleware = require('./middleware.js');
 
-//about 
-//About Us
+app.use(middleware.logger);
 
-app.get('/about',function(req,res){
-	res.send("About Page");
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
-// console.log(__dirname);
 
-//3000 is port
-app.listen(PORT,function(){
-	console.log('Express server started!' + PORT);
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
